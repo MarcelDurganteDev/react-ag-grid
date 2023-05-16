302033ab706a7dce45ac4948585bea0c3621339c
MarcelDurganteDev <info@marceldurgante.dev>
2023-05-16 19:14:41 +0200

    fetched mock data for rowData

--- src/App.js
+++ src/App.js
@@ -28,12 +28,15 @@ class App extends Component {
 					filter: true,
 				},
 			],
-			rowData: [
-				{ make: 'Toyota', model: 'Celica', price: 35000 },
-				{ make: 'Ford', model: 'Mondeo', price: 32000 },
-				{ make: 'Porsche', model: 'Boxter', price: 72000 },
-			],
+			rowData: [],
 		};
+	}
+
+	componentDidMount() {
+		fetch('https://www.ag-grid.com/example-assets/row-data.json')
+			.then((result) => result.json())
+			.then((rowData) => this.setState({ rowData }));
+		console.log('Hello World');
 	}
 
 	render() {
